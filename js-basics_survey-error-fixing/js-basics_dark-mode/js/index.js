583d286f665a9972f6c12b3aab5586533550c64b
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-03-18 11:25:28 +0100

    finished dark mode challenge

--- js-basics_survey-error-fixing/js-basics_dark-mode/js/index.js
+++ js-basics_survey-error-fixing/js-basics_dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+
+const tuggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+tuggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
