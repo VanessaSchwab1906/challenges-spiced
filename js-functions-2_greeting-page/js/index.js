c1f4bd5223fc4d216e2d239d2a4cb1134c7b2d82
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-03-22 11:13:57 +0100

    finished challenge

--- js-functions-2_greeting-page/js/index.js
+++ js-functions-2_greeting-page/js/index.js
@@ -23,10 +23,31 @@ const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
   // Code here
+  const date = new Date().getHours();
+  if (6 <= date <= 12) {
+    return "Good Morning";
+  }
+  if (13 <= date <= 18) {
+    return "Good Afternoon";
+  }
+  if (19 <= date <= 22) {
+    return "Good Evening";
+  }
+  if (23 <= date <= 5) {
+    return "Good Night";
+  }
 }
 
 function getDayColor() {
   // Code here
+  const weekday = new Date().getDay();
+  if (weekday === "monday") {
+    return "darkgray";
+  } else if (weekday === "saturday" || weekday === "sunday") {
+    return "hotpink";
+  } else {
+    return "lightblue";
+  }
 }
 
 display.textContent = getGreeting();
