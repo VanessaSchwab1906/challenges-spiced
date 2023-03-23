635675ffca5bb226cd3480341fa8935853eb0cf6
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-03-23 14:02:13 +0100

    finished challenge

--- js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings_switch-values/js/index.js
@@ -16,11 +16,31 @@ Hint: the HTML entity for the switch button is: &updownarrow
 const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  if (firstInput.value === firstInput.value.toUpperCase()) {
+    firstInput.value = firstInput.value.toLowerCase();
+  } else {
+    firstInput.value = firstInput.value.toUpperCase();
+  }
+});
+
+switchButton.addEventListener("click", () => {
+  if (secondInput.value === secondInput.value.toUpperCase()) {
+    secondInput.value = secondInput.value.toLowerCase();
+  } else {
+    secondInput.value = secondInput.value.toUpperCase();
+  }
 });
