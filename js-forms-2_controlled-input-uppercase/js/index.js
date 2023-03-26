4fb1976e20109a7fd9984d0a908da68651886571
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-03-26 12:51:31 +0200

    finished challenge

--- js-forms-2_controlled-input-uppercase/js/index.js
+++ js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,9 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+input.addEventListener("input", (event) => {
+  const value = event.target.value;
+  //   console.log(value);
+  event.target.value = value.toUpperCase();
+});
