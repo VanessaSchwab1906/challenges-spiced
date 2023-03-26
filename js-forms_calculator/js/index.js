35d316982bce09bf6f6fe0b4e7854fa401bb97e4
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-03-26 15:00:49 +0200

    finished challenge

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -25,6 +25,24 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  // get input from form
+  const formElements = event.target.elements;
+  const a = parseInt(formElements.numberA.value);
+  const b = parseInt(formElements.numberB.value);
+  console.log(a, b);
+  const sum = document.getElementById("addition").checked;
+  const sub = document.getElementById("subtraction").checked;
+  const mult = document.getElementById("multiplication").checked;
+
+  if (sum === true) {
+    result = add(a, b);
+  } else if (sub === true) {
+    result = subtract(a, b);
+  } else if (mult === true) {
+    result = multiply(a, b);
+  } else {
+    result = divide(a, b);
+  }
 
   // --^-- write your code here --^--
 
