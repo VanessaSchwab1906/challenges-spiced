32288b7cb9ab6209761b29a1b0950994a1faa11f
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-13 18:07:41 +0200

    finished challenge

--- react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
+++ react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
@@ -1,16 +1,26 @@
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
+import { useState } from "react";
+
+// Locate the component with the favorite button. Here it's called `FavoriteButton`.
+
+// The component has an `isFavorite` boolean variable. This variable is used to determine if the star icon should be filled in or not.
+
+// - Make `isFavorite` a state variable.
+// - This state variable needs to be toggled when the user clicks the favorite button.
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
+  // const isFavorite = false;
+  const [isFavorite, setIsFavorite] = useState(false);
 
   return (
     <button
       className="favorite-button"
       onClick={() => {
         console.log("favorite button clicked");
+        setIsFavorite(!isFavorite);
       }}
       aria-label="favorite"
     >
