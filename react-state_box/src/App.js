179b3107f7a9c4eff90d3d6ef011a47710687a04
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-13 15:26:52 +0200

    finished challenge

--- react-state_box/src/App.js
+++ react-state_box/src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+import React from "react";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  // let isActive = false;
+  const [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive(!isActive);
     // Check that the value changes correctly.
     console.log(isActive);
   }
