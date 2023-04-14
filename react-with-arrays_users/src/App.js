440921b94e3bf6e3dc68e62695cde51cf4e62be4
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-14 11:33:53 +0200

    finished challenge

--- react-with-arrays_users/src/App.js
+++ react-with-arrays_users/src/App.js
@@ -4,12 +4,18 @@ import Card from "./components/Card";
 import Title from "./components/Title";
 import { USERS } from "./db";
 
+// 1. Use `Array.map` to create a card for each user in the `USERS` array. Make sure to add a unique key to each card.
+
 export default function App() {
   return (
     <main className="app">
       <Title text="ClientBoard" />
       <div className="app__card-grid">
-        <Card user={USERS[0]} />
+        {USERS.map((USER, id) => {
+          return <Card key={id} user={USER} />;
+        })}
+
+        {/* <Card user={USERS[0]} /> */}
       </div>
     </main>
   );
--- react-with-arrays_users/src/components/Tag.js
+++ react-with-arrays_users/src/components/Tag.js
@@ -1,5 +1,9 @@
 import "./Tag.css";
 
+// 3. At last, refactor the `Tag` component: it should receive an additional class called 'Tag--highlight' if the `tag` prop equals the string 'admin' (compare the `Tag.css` file).
+
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  let tagClass = "tag";
+  if (tag === "admin") tagClass = "tag tag--highlight";
+  return <li className={tagClass}>{tag}</li>;
 }
