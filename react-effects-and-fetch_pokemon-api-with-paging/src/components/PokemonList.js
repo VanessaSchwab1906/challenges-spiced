e7786202d3e35f061a30a5c88d0fc10724c2e937
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-17 16:34:18 +0200

    finished challenge

--- react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -1,28 +1,57 @@
 import { useEffect, useState } from "react";
 
+// NEXT BUTTON
+// > _Hint 1:_ You need a way to store which page you are displaying right now and be able to update this value to the next page.
+
+// > _Hint 2:_ Have a look at the URL parameter `?offset=` used for `fetch`. The API starts by giving you the first `20` Pokémon. In the end the URL needs to change. You can either keep track of the offset yourself, or use the value given and `next` in the API response.
+
+// PREV BUTTON
+
+// You might experience that the app crashes when you are on the first page and click on the "Previous Page" button. Can you find a way to hide the button on the first page?
+
+// > _Hint:_ There is also a `previous` value given in the API response.
+
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [offset, settOffSet] = useState(0);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
         );
         const data = await response.json();
         setPokemon(data.results);
+        console.log(data.results);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [offset]);
+
+  function handleNextPage() {
+    settOffSet(offset + 20);
+  }
+
+  function handlePrevPage() {
+    settOffSet(offset - 20);
+  }
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button
+        onClick={handlePrevPage}
+        hidden={offset === 0 ? true : false}
+        type="button"
+      >
+        Previous Page
+      </button>
+      <button onClick={handleNextPage} type="button">
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
