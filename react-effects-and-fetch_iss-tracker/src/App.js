bc288a68b456c9368ae27272135be47115dc8770
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-17 17:27:13 +0200

    finished challenge

--- react-effects-and-fetch_iss-tracker/src/App.js
+++ react-effects-and-fetch_iss-tracker/src/App.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+// 1. Write the function `getISSCoords`. Fetch from the given url saved in the constant `URL`. After fetching successfully update the `coords` state accordingly.
 
 export default function App() {
   const [coords, setCoords] = useState({
@@ -11,7 +14,27 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setCoords({
+        longitude: data.longitude,
+        latitude: data.latitude,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getISSCoords();
+    }, 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
   return (
     <main>
