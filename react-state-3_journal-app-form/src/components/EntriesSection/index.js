c122629ae60e2884ed0653c145c801fb32d46c5b
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-17 13:58:39 +0200

    started challenge but nor working until now

--- react-state-3_journal-app-form/src/components/EntriesSection/index.js
+++ react-state-3_journal-app-form/src/components/EntriesSection/index.js
@@ -6,7 +6,10 @@ import Tab from "../Tab";
 import Badge from "../Badge";
 import { Fragment } from "react";
 
-const entries = [
+// - Rename the variable to `initialEntries` to reflect this is the initial value for the state.
+// - Create a new state called `entries` with `initialEntries` as initial value.
+
+const initialEntries = [
   {
     id: 1000,
     date: "Feb 5, 2025",
@@ -35,8 +38,12 @@ const entries = [
     notes: "My React-ion when I learned about React: Yay!",
   },
 ];
+// - Update the function declaration of the `EntriesSection` component to accept a new prop called `entries`.
+// - Move the `initialEntries` and the state to their common ancestor, e.g. `App`.
+// - Pass the `entries` state via the `entries` prop down to the `EntriesSection`.
 
-export default function EntriesSection() {
+export default function EntriesSection({ entries }) {
+  const [entries, setEntries] = useState(initialEntries);
   return (
     <section className="entries-section">
       <Tabs>
@@ -48,7 +55,7 @@ export default function EntriesSection() {
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
-        {entries.map((entry, index) => (
+        {initialEntries.map((entry, index) => (
           <Fragment key={entry.id}>
             {index > 0 ? <Divider /> : null}
             <Entry date={entry.date} motto={entry.motto} notes={entry.notes} />
--- react-state-3_journal-app-form/src/components/EntryForm/index.js
+++ react-state-3_journal-app-form/src/components/EntryForm/index.js
@@ -1,9 +1,27 @@
 import "./EntryForm.css";
 import Button from "../Button";
 
-export default function EntryForm() {
+// - Update this component's function declaration to accept a new prop, like `onAddEntry`. The prop should be used for an event that you trigger when a new journal entry should be added.
+
+// - Write a function `handleSubmit` and pass it as the `onSubmit` attribute of the `form` JSX tag.
+// - In `handleSubmit` gather the values of all input fields. (Hint: you can use `new FormData()` for this.)
+// - Don't forget to prevent the default form behavior when submitting.
+// - In `handleSubmit` create an object with the keys `motto` and `notes` containing the fields' values.
+// - Call `onAddEntry` and pass the object.
+// - Make sure to reset the form after submitting.
+
+export default function EntryForm({ onAddEntry }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    onAddEntry(data);
+    event.target.reset();
+  }
+
   return (
-    <form className="entry-form">
+    <form className="entry-form" onSubmit={handleSubmit}>
       <h2 className="entry-form__title">New Entry</h2>
       <div className="entry-form__fields">
         <div className="entry-form__field">
