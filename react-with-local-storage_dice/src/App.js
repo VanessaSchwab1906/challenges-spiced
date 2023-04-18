4fd3d1716ffb91fe1fe35f7fe005014eddc7e51f
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-18 11:21:42 +0200

    finished challenge

--- react-with-local-storage_dice/src/App.js
+++ react-with-local-storage_dice/src/App.js
@@ -1,15 +1,37 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./styles.css";
+// import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import { getD6Roll } from "./utils";
+// Lift up the `rolls` state into the `App` component.
+
+// The `App` component should now hold the state, define the `handleRoll` function and derive the `currentRollValue` from the `rolls` state.
+
+// - `handleRoll` should be passed to `onRoll` of the `<D6Button />`
+// - `currentRollValue` should be passed to `value` of the `<D6Button />`
+
+// Make sure everything still works as expected.
+
+// Now pass the `rolls` down to the `History` component as well (replace `rolls={[]}`).
+
+// > ✨ You should now see the list of recent rolls in the History component
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+  const currentRollValue = rolls[0]?.value;
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button onRoll={handleRoll} currentRollValue={currentRollValue} />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls} />
       </aside>
     </div>
   );
--- react-with-local-storage_dice/src/components/D6Button/index.js
+++ react-with-local-storage_dice/src/components/D6Button/index.js
@@ -1,19 +1,26 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
 
-export function D6Button() {
-  const [rolls, setRolls] = useState([]);
+// The `D6Button` component should have the following props:
 
-  const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
-  };
+// - `value` which represents the current roll value (because the button only needs to know the most recent value)
+// - `onRoll` which is a function that get called on roll (when the button is clicked)
 
-  const currentRollValue = rolls[0]?.value;
+// > 💡 When designing the interface of your component (which props it has), think about what the component needs to know to function properly. In this case the component only needs to know the current value it should show, and a function that gets called when the button is clicked. Hence you should not pass the whole `rolls` array to the component.
+
+export function D6Button({ currentRollValue, onRoll }) {
+  // const [rolls, setRolls] = useState([]);
+
+  // const handleRoll = () => {
+  //   setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  // };
+
+  // const currentRollValue = rolls[0]?.value;
 
   return (
-    <button className="d6-button" type="button" onClick={handleRoll}>
+    <button className="d6-button" type="button" onClick={onRoll}>
       <D6 value={currentRollValue} />
     </button>
   );
