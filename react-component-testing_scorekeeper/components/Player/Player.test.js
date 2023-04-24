7d141dceb70cf37b2b4a20a194cd3a037b4c8a44
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-04-24 15:26:36 +0200

    add test for Player and GameForm

--- react-component-testing_scorekeeper/components/Player/Player.test.js
+++ react-component-testing_scorekeeper/components/Player/Player.test.js
@@ -6,6 +6,29 @@ import Player from ".";
 // - To check the callback functions of a component, remember to mock them first and then pass the mocked function to the rendered component.
 // - Your `user.click` does not call the mocked functions properly? Don't forget to use `await`.
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  render(<Player name="Vanessa" score={0} />);
+  const player = screen.getByText("Vanessa");
+  const score = screen.getByText("0");
+  const buttons = screen.getAllByRole("button");
 
-test("calls callbacks when increasing or decreasing score", async () => {});
+  expect(player).toBeInTheDocument();
+  expect(score).toBeInTheDocument();
+  expect(buttons).toHaveLength(2);
+});
+
+test("calls callbacks when increasing or decreasing score", async () => {
+  const onIncrease = jest.fn();
+  const onDecrease = jest.fn();
+  const user = userEvent.setup();
+
+  render(
+    <Player name="Vanessa" score={0} onIncreasePlayerScore={onIncrease} />
+  );
+  const increaseButton = screen.getByRole("button", { name: "Increase Score" });
+  const decreaseButton = screen.getByRole("button", { name: "Decrease Score" });
+  await user.click(increaseButton);
+  expect(onIncrease).toHaveBeenCalledTimes(1);
+  await user.click(decreaseButton);
+  expect(onDecrease).toHaveBeenCalledTimes(0);
+});
