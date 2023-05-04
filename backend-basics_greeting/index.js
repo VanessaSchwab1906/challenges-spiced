acd0992abfaba370eef5e393fb6fb5ea498b7fb3
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-05-04 10:18:12 +0200

    finished challenge

--- backend-basics_greeting/index.js
+++ backend-basics_greeting/index.js
@@ -1,3 +1,9 @@
-// import { server } from "./server.js";
+// - Import the server from the [`server.js`](./server.js) file.
+// - Call the `.listen()` method on the server and pass it a port number you want to listen to, e.g. 8000.
 
-// server.listen(…
+import { server } from "./server.js";
+
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server runnin at http:127.0.0.1:${port}/`);
+});
--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,3 +1,11 @@
+// - Import the `createServer` method from the `http` module.
+// - Call `createServer` and pass it a function with two parameters: `request` and `response`.
+// - In the function body of the function passed to `createServer`, call `response.end()` and pass it the string "Hello, [your name]!".
+// - Export the server as a named export.
+
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  response.statusCode = 200;
+  response.end("Hello Vanessa");
+});
