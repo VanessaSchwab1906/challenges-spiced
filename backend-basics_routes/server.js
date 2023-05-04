aedc5d497d43301bd6336cc95fe7eec8f3e096bf
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-05-04 10:47:37 +0200

    finished challenge

--- backend-basics_routes/server.js
+++ backend-basics_routes/server.js
@@ -1,3 +1,22 @@
 import { createServer } from "node:http";
 
-// export const server = …
+// - if the requested url is `/api/fish/1`, the response should be `"Shrimp"` and the status code should be `200`;
+// - if the requested url is `/api/fish/2`, the response should be `"Anemonefish"` and the status code should be `200`;
+// - for all other urls, the response should be `"Not found"` and the status code should be `404`.
+
+export const server = createServer((request, response) => {
+  switch (request.url) {
+    case "/api/fish/1":
+      response.statusCode = 200;
+      response.end("Shrimp");
+      break;
+    case "/api/fish/2":
+      response.statusCode = 200;
+      response.end("Anemonefish");
+      break;
+    default:
+      response.statusCode = 404;
+      response.end("Not Found");
+      break;
+  }
+});
