bfdf62ad40709d7157733f16d564bfeb4f246dbf
Vanessa Schwab <vanessa-schwab@gmx.de>
2023-05-04 11:10:55 +0200

    finished challenge

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,14 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((request, response) => {
+  response.statusCode = 200;
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+  response.end(
+    `Hello my name is ${name} and I am ${age} years old. I am a ${profession}.`
+  );
+});
